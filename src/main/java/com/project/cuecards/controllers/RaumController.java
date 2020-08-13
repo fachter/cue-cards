8ce59e7d2d4080788cafeb294beb2e0b5ce18eda
Felix Achter <felix.achter@achter-beratung.de>
2020-08-13 13:29:34 +0200

    RaumController

--- src/main/java/com/project/cuecards/controllers/RaumController.java
+++ src/main/java/com/project/cuecards/controllers/RaumController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
+@RestController
 public class RaumController {
 
     @GetMapping("/get-available-rooms")
